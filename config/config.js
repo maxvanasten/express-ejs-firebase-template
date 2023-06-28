f54cf61525324755cd1afb593b86240890b34f35
Max van Asten <maxvasten@gmail.com>
2023-06-29 00:53:58 +0200

    Added configuration files and service account

---
+++ config/config.js
@@ -0,0 +1,8 @@
+/** @format */
+
+const config = {
+  port: 3000,
+  debug: true, // Enables/disables debugLog()
+};
+
+export { config };
---
+++ config/serviceAccountKey.js
@@ -0,0 +1,7 @@
+/** @format */
+
+const serviceAccount = {
+  // Insert service account stuff from project settings -> service accounts -> firebase admin sdk -> generate new private key
+};
+
+export { serviceAccount };
